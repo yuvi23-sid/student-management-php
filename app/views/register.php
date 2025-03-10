b1fbe0bca12964032a9cae41752ade35c96735ac
Yuvraj Sidhu <147128567+yuvi23-sid@users.noreply.github.com>
2025-03-10 17:59:42 -0600

    some css changes

--- app/views/register.php
+++ app/views/register.php
@@ -16,7 +16,7 @@
         }
         .container {
             background-color: #fff;
-            padding: 20px;
+            padding: 35px;
             border-radius: 8px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
             width: 300px;
